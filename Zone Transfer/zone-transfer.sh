910465de76d968eb61dc9b753a523dcad352a6a1
Dhiyaneshwaran <leedhiyanesh@gmail.com>
2019-10-08 22:35:39 +0530

    Add files via upload

---
+++ Zone Transfer/zone-transfer.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+#Simple Zone Transfer Bash Script
+
+if [ -z "$1" ]; then
+	echo "[*] Simple Zone Transfer Script"
+	echo "[*] Usage : $0 <domain name>"
+	exit 0
+fi
+
+for server in $(host -t ns $1 |cut -d" " -f4);do
+	host -l $1 $server |grep "has address"
+done
